e63f0696fd24fabb5743f81c489f10b0c6d62979
544-eth <elioku20@gmail.com>
2025-02-04 22:20:45 +0100

    INC

--- UI.js
+++ UI.js
@@ -2,13 +2,39 @@ export class UI {
     constructor(game) {
         this.game = game
         this.fontSize = 30
-        this.fontFamily = 'sans' 
+        this.fontFamily = 'Creepster' 
+        this.livesImage = document.getElementById('lives')
     }
     draw(context){
+        context.save() 
+        context.shadowOffsetX = 3
+        context.shadowOffsetyY = 3
+        context.shadowColor = 'blue'
+        context.shadowBlur = 0
         context.font = this.fontSize + 'px ' + this.fontFamily
         context.textAlign = 'left'
         context.fillStyle = this.game.fontColor
         // score
         context.fillText('Score: ' + this.game.score, 20, 45)
+        // timer   
+        context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily
+        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 20, 80)
+        // Lives
+        context.drawImage(this.livesImage, 20, 95, 25, 25)
+        // game over messages
+        if (this.game.gameOver){
+            context.textAlign = 'center'
+            context.font = this.fontSize * 2 + 'px ' + this.fontFamily
+            if (this.game.score > 5){
+                context.fillText('Excellent', this.game.width * 0.5, this.game.height * 0.5 - 20)
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily
+                context.fillText('Yeah keep it up ', this.game.width * 0.5, this.game.height * 0.5 + 20)
+            } else {
+                context.fillText('why!!! ', this.game.width * 0.5, this.game.height * 0.5 - 20)
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily
+                context.fillText('Well, give it another try ', this.game.width * 0.5, this.game.height * 0.5 + 20)
+            }
+        }
+        context.restore()
     }
 }
